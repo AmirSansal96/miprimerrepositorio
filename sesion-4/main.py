2c537407fe50cde59e7ff5008b1a718ae829b9a9
AmirSansal96 <amirsanchezs@outlook.com>
2023-08-11 12:09:10 -0600

    actualice calculadora

--- sesion-4/main.py
+++ sesion-4/main.py
@@ -1,6 +1,7 @@
 def calculadora():
+      
     print("Hola bienvenido a la calculadora")
-    op=input("Que operacion quieres ejecutar?(suma/resta)")
+    op=input("Que operacion quieres ejecutar?(suma/resta/multiplicacion/division)")
     if op == "suma":
         primer_numero = int(input("dame tu primer numero"))
         segundo_numero = int(input("dame tu segundo numero"))
@@ -9,4 +10,14 @@ def calculadora():
         primer_numero = int(input("dame tu primer numero"))
         segundo_numero = int(input("dame tu segundo numero"))
         print(f"tu numero es: {primer_numero - segundo_numero}")
+    elif op == "multiplicacion":
+        primer_numero = int(input("dame tu primer numero"))
+        segundo_numero = int(input("dame tu segundo numero"))
+        print(f"tu numero es: {primer_numero * segundo_numero}")
+    elif op == "division":
+        primer_numero = int(input("dame tu primer numero"))
+        segundo_numero =int(input("dame tu segundo numero"))
+        print(f"tu numero es: {primer_numero / segundo_numero}")
+
        
+calculadora()
